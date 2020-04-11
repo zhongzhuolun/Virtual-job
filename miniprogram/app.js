b8ff8999a9284089556ced98a134364e479358d8
zhongzhuolun <781568228@qq.com>
2020-04-11 21:50:55 +0800

    init
    init

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -16,5 +16,13 @@ App({
     }
 
     this.globalData = {}
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let custom = wx.getMenuButtonBoundingClientRect();
+        this.globalData.Custom = custom;  
+        this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+      }
+    })
   }
 })
